d0de2e013ec7f977d7ccc53be4ef4690d720cda4
Fredrik Eriksson <git@wb9.se>
2015-11-08 10:05:46 +0100

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from os import environ
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils import setup
+
+import adbb
+
+version = float(adbb.anidb_client_version)/10
+
+setup(
+        name='adbb',
+        version=str(version),
+        description="Object Oriented AniDB UDP Client Library",
+        author="Winterbird",
+        author_email="adbb<_at_>winterbird.org",
+        url='https://github.com/winterbird-code/adbb',
+        platforms=['any'],
+        license= "GPLv3",
+        packages=['adbb'])
