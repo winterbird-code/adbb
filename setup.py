ea211fca58d24ff21bb9aa5a950fa6fe31977d6e
Winterbird <winterbird-code@winterbird.org>
2023-09-11 17:05:15 +0200

    bump release version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(readme, 'r', encoding='utf-8') as f:
 
 setup(
         name='adbb',
-        version=str(version),
+        version=f'{version:.1f}.1',
         description="Object Oriented AniDB UDP Client Library",
         long_description_content_type="text/markdown",
         long_description=long_description,
